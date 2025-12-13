e19e5d33c68fae81c7ab8b711ac27aa5b9e9a28e
saju-79 <saju98622@gmail.com>
2025-12-13 12:33:31 -0800

    friend

--- friend.js
+++ friend.js
@@ -1,6 +1,22 @@
-function(friend1 , friend2){
-    console.log(friend1 , friend2)
-}
-
 const friend1 ={name:"saju ahmed" , rool:1 ,bestFriendNumber:2 }
 const friend2 ={name:"mubin tahmed " , rool:2 ,bestFriendNumber:1 }
+function bestFriend(a ,b){
+    
+        if(a.rool === b.bestFriendNumber && a.bestFriendNumber === b.rool){
+           return"asi"
+        }else{
+            return"naiii"
+        }
+    
+
+    }
+          
+        
+    
+ 
+
+
+ 
+const outpu = bestFriend(friend1 , friend2)
+console.log(outpu)
+
