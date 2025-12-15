917c2058052fb41fb1002414a798d56e9f1ef473
saju-79 <saju98622@gmail.com>
2025-12-15 10:20:05 -0800

    delte-some-comment

--- watch_time.js
+++ watch_time.js
@@ -1,31 +1,4 @@
-
-// const totalTimes= [ 400 ,200 ,300];
-// function calculateWatchTime(times) {
-//   if (!Array.isArray(times)) return "Invalid";
-
-//   let totalSeconds = 0;
-
-//   for (let time of times) {
-//     if (typeof time !== "number") {
-//       return "Invalid";
-//     }
-//     totalSeconds += time;
-//   }
-
-//   let hour = Math.floor(totalSeconds / 3600);
-//   let minute = Math.floor((totalSeconds % 3600) / 60);
-//   let second = totalSeconds % 60;
-
-//   return {
-//     hour: hour,
-//     minute: minute,
-//     second: second
-//   };
-// }
-
-// const output = calculateWatchTime(totalTimes);
-// console.log(output)
-// console.log(Math.floor(300/3600))
+ 
 
 
 const timeSecound = [2323, 3600 ,60 ,60 ,30];
