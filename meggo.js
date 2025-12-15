1d01c19547fdeb354e4f6002e8f7ede7288a317a
saju-79 <saju98622@gmail.com>
2025-12-15 10:15:33 -0800

    manngo

--- meggo.js
+++ meggo.js
@@ -1,16 +1,36 @@
- const food =["mango" , "banana" ,"non-vesitable" , "mango"]
- function electionResult(foods){
-  for (let i = 0; i < foods.length; i++) {
-    const element = foods[i];
-    let food = element
-    if(element.length > food.length){
-        food = element 
-        break
-    }
-    
+ const food =["banana" , "banana" , "banana" ,"non-vesitable" , "mango"]
+function electionResult(votes) {
+  if (!Array.isArray(votes)) {
+    return "Invalid";
   }
-  return food;
+
+  let mango = 0;
+  let banana = 0;
+
+ for(const vote of votes){
+       const text = vote.toLowerCase()
+       if(text === "mango"){
+        mango++
+       }
+      else if(text === "banana"){
+        banana++
+      }
+
  }
 
- const output = electionResult(food);
+ console.log(banana , mango)
+ if(mango >banana){
+    return "mango"
+} else if(banana >mango){ 
+  return "banana"
+}
+else{
+  return 'Draw '
+}
+}
+const output = electionResult(food)
  console.log(output)
+
+
+
+ 
